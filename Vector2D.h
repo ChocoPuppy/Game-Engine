38b31d70958ed44b3a376768bb60a2c42b4fcb5a
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-07-01 13:24:41 +1000

    Added constructor to Vector2D to initialize the struct with values.

--- Vector2D.h
+++ Vector2D.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <utility>
-struct Vector2D : std::pair<double,double>
+struct Vector2D : std::pair<double, double>
 {
+	Vector2D( double x = 0, double y = 0 ) : std::pair<double, double>{ x,y } {}
+
 	double & x();
 	double & y();
 };
