95a0cb27d4033b3d3250841f7aa0f47a8766b7af
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-07-08 15:30:32 +1000

    Updated Dino and WalkingDino to use the function version of GameObject::transform.

--- Dino.cpp
+++ Dino.cpp
@@ -1,11 +1,11 @@
 #include "Dino.h"
 
-Dino::Dino() : GameObject("Dino","Texture.Dino")
+Dino::Dino() : GameObject( "Dino", "Texture.Dino" )
 {
-	transform.position.x() = 25;
-	transform.position.y() = 25;
-	transform.scale.x() = 100;
-	transform.scale.y() = 100;
+	transform().position.x() = 25;
+	transform().position.y() = 25;
+	transform().scale.x() = 100;
+	transform().scale.y() = 100;
 }
 
 void Dino::simulateAI( unsigned long, AssetManager * )
--- WalkingDino.cpp
+++ WalkingDino.cpp
@@ -1,12 +1,14 @@
 #include "WalkingDino.h"
 
-WalkingDino::WalkingDino() : AnimatedGameObject("WalkingDino","Texture.Dino.Walking")
+WalkingDino::WalkingDino() : AnimatedGameObject( "WalkingDino", "Texture.Dino.Walking" )
 {
-	transform.position.x() = 75;
-	transform.position.y() = 75;
-	transform.scale.x() = 100;
-	transform.scale.y() = 100;
+	transform().position.x() = 75;
+	transform().position.y() = 75;
+	transform().scale.x() = 100;
+	transform().scale.y() = 100;
+
+	velocity() = Vector2D( 0.02f, 0 );
 }
 
-void WalkingDino::simulateAI( unsigned long , AssetManager * )
+void WalkingDino::simulateAI( unsigned long, AssetManager * )
 {}
