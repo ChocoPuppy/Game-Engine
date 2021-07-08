b5a0d2514c18156e304124eea4dfb585830750db
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-07-08 14:43:29 +1000

    Added operator definitions to Vector2D.

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -1,5 +1,5 @@
 #include "Vector2D.h"
-
+#include <math.h>
 double const & Vector2D::x() const
 {
 	return first;
@@ -18,4 +18,49 @@ double const & Vector2D::y() const
 double & Vector2D::y()
 {
 	return const_cast<double &>( const_cast<Vector2D const *>( this )->y() );
+}
+
+float Vector2D::magnitude() const
+{
+	return sqrtf( x() * x() + y() * y() );
+}
+
+Vector2D Vector2D::normalize() const
+{
+	Vector2D normalizedVector;
+	float mag = magnitude();
+
+	if (mag > 0)
+	{
+		normalizedVector.x() /= mag;
+		normalizedVector.y() /= mag;
+	}
+}
+
+void Vector2D::operator+=( Vector2D const & rhs )
+{
+	x() += rhs.x();
+	y() += rhs.y();
+}
+
+void Vector2D::operator-=( Vector2D const & rhs )
+{
+	x() -= rhs.x();
+	y() -= rhs.y();
+}
+
+void Vector2D::operator*=( float rhs )
+{
+	x() *= rhs;
+	y() *= rhs;
+}
+
+Vector2D operator+( Vector2D lhs, Vector2D const & rhs )
+{
+	return lhs += rhs;
+}
+
+Vector2D operator-( Vector2D lhs, Vector2D const & rhs )
+{
+	return lhs -= rhs;
 }
