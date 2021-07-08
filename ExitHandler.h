417ed2a50bab3486ab4e275c12e6d4104582dfde
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-07-08 15:25:21 +1000

    ExitHandler now knows what event it's responding to.

--- ExitHandler.h
+++ ExitHandler.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Event/SmartEvent/ASmartObserver.h"
+#include "ButtonEvent.h"
 class ExitHandler : Event::SmartEvent::ASmartObserver<ButtonPressedEvent>
 {
 	virtual void update( InputManager * manager, const Button button ) override;
