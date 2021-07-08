0632b8e91e8184bd9bb124a1d437fd1fd0162647
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-07-08 16:05:03 +1000

    Changed if statement in exitHandler to switch statement since it can cover the situation just fine.

--- ExitHandler.cpp
+++ ExitHandler.cpp
@@ -2,7 +2,12 @@
 
 void ExitHandler::update( InputManager *, const Button button )
 {
-	//If it's not the button we're looking for, exit immediatly.
-	if (button != Button::QUIT) return;
-	exit( 0 );
+	switch (button)
+	{
+	case Button::QUIT:
+		exit( 0 );
+		break;
+	default:
+		return;
+	}
 }
