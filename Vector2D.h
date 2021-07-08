2c9da1d130096ed6fbf7ff0f219eee5316a2a98e
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-07-08 16:21:29 +1000

    Made Vector2D's constructor constexpr since it qualifies.

--- Vector2D.h
+++ Vector2D.h
@@ -2,7 +2,7 @@
 #include <utility>
 struct Vector2D : std::pair<double, double>
 {
-	Vector2D( double x = 0, double y = 0 ) : std::pair<double, double>{ x,y } {}
+	constexpr Vector2D( double x = 0, double y = 0 ) : std::pair<double, double>{ x,y } {}
 
 	double const & x() const;
 	double & x();
