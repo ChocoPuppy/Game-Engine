52b7ac1bbaafa0cc06d4ccd2bf6c231a07260c6e
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-09 23:22:46 +1000

    Made dino slowly fall and rotate down the screen partially as a test of the physics and also because I find it funny.

--- Dino.cpp
+++ Dino.cpp
@@ -6,6 +6,9 @@ Dino::Dino() : GameObject( "Dino", "Texture.Dino" )
 	transform().position.y() = 25;
 	transform().scale.x() = 100;
 	transform().scale.y() = 100;
+
+	velocity().y() = 0.0000000001;
+	rotationalVelocity() = 0.1f;
 }
 
 void Dino::simulateAI( unsigned long, AssetManager * )
