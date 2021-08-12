d406036f5dfc1d1fcbfad990548a6492d5475239
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-12 22:40:21 +1000

    Modified the SDL Wrapper. I really should probably move this to an include. Made the SDLBase and SDLVideo automatically initialize and destroy their respective SDL subsystems if included.

--- SDLVideo.h
+++ SDLVideo.h
@@ -1,5 +1,29 @@
 #pragma once
-#include "SDLMain.h"
-#include "Renderer.h"
-#include "Window.h"
-#include "Texture.h"
+#include "SDLBase.h"
+
+//Initialize the video subsystem at startup if included and quit it at program end (object destruction)
+namespace
+{
+	struct _SDLVideo
+	{
+		_SDLVideo()
+		{
+			SDL_InitSubSystem( SDL_INIT_VIDEO );
+		}
+
+		~_SDLVideo()
+		{
+			SDL_QuitSubSystem( SDL_INIT_VIDEO );
+		}
+	};
+
+	_SDLVideo _sdlVideo = _SDLVideo();
+}
+
+namespace SDL
+{
+	class Texture;
+	class Window;
+	class WindowBuilder;
+	class Renderer;
+}
