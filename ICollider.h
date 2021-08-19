1715a2af098765412db1737a41e772d230485864
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-19 23:05:16 +1000

    Added ICollider, an interface for any Colliders to inherit off.

---
+++ ICollider.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Vector2D.h"
+#include "Rect2D.h"
+#include <string>
+struct Transform2D;
+namespace Collision
+{
+	class ICollider
+	{
+		bool _isTrigger;
+		Rect2D _extentsOfCollider;
+	protected:
+	public:
+		virtual std::string getTextureID() const = 0;
+		/// @brief Returns the minimum size of rectangle that can fit the entire collider in it.
+		/// @return The smallest possible rectangle that can fit the collider in it.
+		virtual Rect2D extentsOfCollider() const = 0;
+
+		virtual void render( Transform2D const & transform ) const;
+		bool isTrigger() const { return _isTrigger; }
+		void setIsTrigger( bool value ) { _isTrigger = value; }
+
+		//		virtual float intersectionDepth( Vector2D point ) const = 0;
+				//		virtual Vector2D closestEdgeToPoint( Vector2D point ) const = 0;
+	};
+}
