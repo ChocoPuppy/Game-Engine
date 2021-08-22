3f43c173f674495b05a3e530fb756f8814ff9626
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-22 20:27:37 +1000

    Removed unecessary override of render in RectCollider2D.

--- RectCollider2D.h
+++ RectCollider2D.h
@@ -5,7 +5,6 @@ namespace Collision
 	class RectCollider2D : public ICollider
 	{
 		virtual std::string getTextureID() const override;
-		virtual void render( Transform2D const & transform, AssetManager const & assets, RenderEngine const & renderer ) const override;
 
 		virtual Rect2D extentsOfCollider() const override;
 	};
