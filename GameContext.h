06cb1288f6f4c1a92b28234da7468944c3877033
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-22 21:45:08 +1000

    Added PhysicsEngine to GameContext.

--- GameContext.h
+++ GameContext.h
@@ -2,16 +2,19 @@
 class AssetManager;
 class Scene;
 class InputManager;
+class PhysicsEngine;
 struct GameContext
 {
 private:
 	AssetManager * _assets;
 	Scene * _scene;
 	InputManager * _inputManager;
+	PhysicsEngine * _physicsEngine;
 public:
-	inline GameContext( AssetManager * assets, Scene * scene, InputManager * inputManager ) : _assets( assets ), _scene( scene ), _inputManager( inputManager ) {}
+	inline GameContext( AssetManager * assets, Scene * scene, InputManager * inputManager, PhysicsEngine * physicsEngine ) : _assets( assets ), _scene( scene ), _inputManager( inputManager ), _physicsEngine( physicsEngine ) {}
 
 	inline AssetManager * getAssets() noexcept { return _assets; };
 	inline Scene * getScene() noexcept { return _scene; };
 	inline InputManager * getInputManager() noexcept { return _inputManager; }
+	inline PhysicsEngine * getPhysicsEngine() noexcept { return _physicsEngine; }
 };
--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 #include "InputManager.h"
 #include "GameScene.h"
 #include "RenderEngine.h"
+#include "PhysicsEngine.h"
 int main()
 {
 	GameEngine engine = GameEngine();
-
 	AssetManager assets = AssetManager( static_cast<SDL::Renderer *>( engine.getRenderer() ) );
 	InputManager inputManager = InputManager();
-
 	Scene * gameScene = new GameScene();
+	PhysicsEngine physicsEngine = PhysicsEngine();
 
-	GameContext context = GameContext( &assets, gameScene, &inputManager );
+	GameContext context = GameContext( &assets, gameScene, &inputManager, &physicsEngine );
 	while (true)
 	{
-		engine.update( context, &inputManager );
+		engine.update( context );
 	}
 
 	return 0;
