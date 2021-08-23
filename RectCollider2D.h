5387bab87ee9ac15243d1811ab80ef6f7aea8e81
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-23 20:54:00 +1000

    Added definitions for most RectCollider2D functions.

--- RectCollider2D.h
+++ RectCollider2D.h
@@ -1,11 +1,25 @@
 #include "ICollider.h"
-
+#include "CollisionTester.h"
 namespace Collision
 {
 	class RectCollider2D : public ICollider
 	{
+		Rect2D _rect;
+	public:
 		virtual std::string getTextureID() const override;
 
 		virtual Rect2D extentsOfCollider() const override;
+
+		Rect2D getSize() const;
+		void setSize( Rect2D size );
+	};
+	template<>
+	class TestForSpecificOverlap<RectCollider2D, RectCollider2D>
+	{
+		//Since the generic test is the same as this, don't actually bother testing. This is just explicitly stated so people don't get confused.
+		bool test( RectCollider2D const & colliderA, RectCollider2D const & colliderB, CollisionData & data )
+		{
+			return true;
+		}
 	};
 }
