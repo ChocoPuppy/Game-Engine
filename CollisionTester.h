6916f90c5d5717397ccaa0e8fb9d4c538b468bb0
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-23 18:56:27 +1000

    TestSpecificOverlap now returns a bool on whether it successfully found a specific test or not. CollisionData is now an out parameter of the function.

--- CollisionTester.h
+++ CollisionTester.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "Vector2D.h"
 #include "ICollider.h"
-#include "RectCollider2D.h"
 #include <type_traits>
 namespace Collision
 {
@@ -10,7 +9,7 @@ namespace Collision
 		float intersectDistance;
 	};
 
-	/// @brief A std::hash-like class for specialized overlap checking functions. This default implementation can't be called.
+	/// @brief A std::hash-like class for specialized overlap checking functions. This default implementation can't be called and will be skipped.
 	/// @tparam ColliderA
 	/// @tparam ColliderB
 	template<class ColliderA, class ColliderB>
@@ -22,7 +21,7 @@ namespace Collision
 		void operator=( TestForSpecificOverlap const & ) = delete;
 		void operator=( TestForSpecificOverlap && ) = delete;
 
-		CollisionData test( ColliderA const & colliderA, ColliderB const & colliderB ) = delete;
+		bool test( ColliderA const & colliderA, ColliderB const & colliderB, CollisionData & data ) = delete;
 	};
 
 	namespace
@@ -30,26 +29,34 @@ namespace Collision
 		template<class ColA, class ColB>
 		using DoesSpecificOverlapTestForPairExist = std::is_default_constructible<TestForSpecificOverlap<ColA, ColB>>;
 	}
-	//If there's no overload for TestForSpecificOverlap then just return blank data.
+	/// @brief If there's no specific overlap test for the types of those colliders, don't overwrite data.
+	/// @tparam ColA One of the collider types.
+	/// @tparam ColB The other collider type.
+	/// @param colA One of the colliders.
+	/// @param colB The other collider.
+	/// @param data If return is true, will be overwritten with the collision data. Otherwise nothing happens to this
+	/// @return If a specific overlap test for those collider types exists.
 	template<class ColA, class ColB>
-	typename std::enable_if_t<( !DoesSpecificOverlapTestForPairExist<ColA, ColB>::value && !DoesSpecificOverlapTestForPairExist<ColB, ColA>::value ), CollisionData>
-		testSpecificOverlap( ColA const & colA, ColB const & colB )
+	typename std::enable_if_t<( !DoesSpecificOverlapTestForPairExist<ColA, ColB>::value && !DoesSpecificOverlapTestForPairExist<ColB, ColA>::value ), bool>
+		testSpecificOverlap( ColA const & colA, ColB const & colB, CollisionData & data )
 	{
-		return CollisionData{};
+		return false;
 	}
 	//Otherwise run the overlap test and return that.
 	template<class ColA, class ColB>
-	typename std::enable_if_t<DoesSpecificOverlapTestForPairExist<ColA, ColB>::value, CollisionData>
-		testSpecificOverlap( ColA const & colA, ColB const & colB )
+	typename std::enable_if_t<DoesSpecificOverlapTestForPairExist<ColA, ColB>::value, bool>
+		testSpecificOverlap( ColA const & colA, ColB const & colB, CollisionData & data )
 	{
-		return TestForSpecificOverlap<ColA, ColB>.test( colA, colB );
+		data = TestForSpecificOverlap<ColA, ColB>.test( colA, colB );
+		return true;
 	}
 
 	template<class ColA, class ColB>
-	typename std::enable_if_t<DoesSpecificOverlapTestForPairExist<ColB, ColA>::value, CollisionData>
-		testSpecificOverlap( ColA const & colA, ColB const & colB )
+	typename std::enable_if_t<DoesSpecificOverlapTestForPairExist<ColB, ColA>::value, bool>
+		testSpecificOverlap( ColA const & colA, ColB const & colB, CollisionData & data )
 	{
-		return TestForSpecificOverlap<ColB, ColA>.test( colB, colA );
+		data = TestForSpecificOverlap<ColB, ColA>.test( colB, colA );
+		return true;
 	}
 
 	CollisionData testForGenericOverlap( ICollider const & colA, ICollider const & colB );
@@ -59,10 +66,10 @@ namespace Collision
 	//	Collision rectOverlap( ColliderAtPosition const & colA, ColliderAtPosition const & colB );
 
 	/// @brief Tests if two colliders are overlapping. If the bounding boxes of the colliders aren't overlapping, the specific overlap tests aren't run.
-	/// @tparam ColA
-	/// @tparam ColB
-	/// @param colA
-	/// @param colB
+	/// @tparam ColA One of the collider types.
+	/// @tparam ColB The other collider type.
+	/// @param colA One of the colliders.
+	/// @param colB The other collider.
 	/// @return Data about the overlap (or lack thereof).
 	template<class ColA, class ColB>
 	CollisionData testForOverlap( ColA const & colA, ColB const & colB )
@@ -70,11 +77,8 @@ namespace Collision
 		CollisionData result = testForGenericOverlap( colA, colB );
 		if (result.intersectDistance != 0)
 		{
-			CollisionData specificResult = testSpecificOverlap<ColA, ColB>( colA, colB );
-			if (specificResult.intersectDistance != 0)
-			{
-				result = specificResult;
-			}
+			//If a specific overlap test exists, results will be overwritten with that value, otherwise nothing happens.
+			testSpecificOverlap<ColA, ColB>( colA, colB, result );
 		}
 		return result;
 	}
