7942e1b7e98caf1e2dbb74b58309ceba5cd73c14
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-30 19:54:04 +1000

    Added conditional statement in GameObject::render to render debug collider sprites underneath the sprite if Config::displayColliders is true.

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,6 +1,7 @@
 #include "GameObject.h"
 #include "TextureWrapper.h"
 #include "RenderEngine.h"
+#include "Config.h"
 GameObject::GameObject( std::string ID, std::string textureID ) : _ID( ID ), _textureID( textureID )
 {}
 
@@ -19,5 +20,17 @@ std::string GameObject::textureID()
 
 void GameObject::render( unsigned long, AssetManager * assets, RenderEngine * renderer )
 {
+	if (Config::displayColliders)
+	{
+		Transform2D inheritedTransform = *transform();
+		inheritedTransform.position = getCollider()->getWorldPosition();
+		//		constexpr float ANTI_THICC_COEFFICIENT = 0.01f;
+		inheritedTransform.rotation = 0;
+		inheritedTransform.scale.x /= getCollider()->extentsOfCollider().getSize().x;
+		inheritedTransform.scale.y /= getCollider()->extentsOfCollider().getSize().y;
+
+		renderer->renderTexture( *transform(), *assets->getAsset<Texture>( getCollider()->getTextureID() ) );
+	}
+
 	renderer->renderTexture( *transform(), *assets->getAsset<Texture>( textureID() ) );
 }
