e86d8ad5d570834c850db8721b519acd690f3725
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-08-30 20:00:32 +1000

    Made a variety of functions in Vector2D constexpr and noexcept since they qualify. Replaced Vector2D::operator*=() with scalar() crossProduct() and dotProduct() since there are multiple ways to multiply a vector. Note I didn't remove operator* as there is only one operation for multiplying a Vector2D and a number, so I might as well just keep that shortcut since it makes sense. Removed operator/=() and operator/() because I realised there is no definite definition for dividing a vector, so to remove confusion, I just got rid of it.

--- Vector2D.h
+++ Vector2D.h
@@ -2,35 +2,61 @@
 #include <utility>
 struct Vector2D : std::pair<double, double>
 {
-	constexpr Vector2D( double x = 0, double y = 0 ) : std::pair<double, double>{ x,y } {}
+	constexpr Vector2D( double x = 0, double y = 0 ) noexcept : std::pair<double, double>{ x,y }, x( first ), y( second ) {}
+	constexpr Vector2D( Vector2D const & vector2D ) noexcept : Vector2D( vector2D.x, vector2D.y ) {}
+	constexpr Vector2D( Vector2D && vector2D ) noexcept : Vector2D( vector2D.x, vector2D.y ) {}
 
-	double const & x() const;
-	double & x();
-	double const & y() const;
-	double & y();
+	constexpr Vector2D & operator=( Vector2D const & vector2D ) noexcept { x = vector2D.x; y = vector2D.y; return *this; }
 
-	float magnitude() const;
-	Vector2D normalize() const;
+	double & x;
+	double & y;
 
-	Vector2D & operator+=( Vector2D const & rhs );
-	Vector2D & operator-=( Vector2D const & rhs );
-	Vector2D & operator*=( float rhs );
-	Vector2D & operator/=( float rhs );
+	constexpr Vector2D normalize() const
+	{
+		Vector2D normalizedVector{ x,y };
+		const float mag = magnitude();
 
-	/// @brief This non-assign operator isn't a friend like the others because it's sided (If you swap the two inputs to the opposite sides of the operator the output will change)
-	/// @param rhs The reason why this function only has one paramter is due to the linker only checking the left hand side (or, if that fails, global namespace overrides, which is what those friend functions are) for relavent overrides, thus meaning you can't do float / Vector2D (since that results in a different output than Vector2D / float).
-	/// @return A copy of this with all values divided by rhs.
-	Vector2D operator/( float rhs );
+		if (mag > 0)
+		{
+			normalizedVector.x /= mag;
+			normalizedVector.y /= mag;
+		}
+		return normalizedVector;
+	}
 
-	friend Vector2D operator+( Vector2D lhs, Vector2D const & rhs );
-	friend Vector2D operator-( Vector2D lhs, Vector2D const & rhs );
-	friend Vector2D operator-( Vector2D lhs );
+	constexpr Vector2D abs() const { return { std::abs( x ),std::abs( y ) }; }
 
-	/// @brief An unsided non-assigning multiplier.
+	constexpr float magnitude() const
+	{
+		return sqrtf( (float)( x * x + y * y ) );
+	}
+
+	constexpr float distance( Vector2D point ) const noexcept
+	{
+		const Vector2D difference = *this - point;
+		return difference.magnitude();
+	}
+
+	constexpr Vector2D scalar( float scale ) const noexcept { return { x * scale, y * scale }; }
+	/// @brief Cross product of this and rhs.
+	/// @param rhs The other vector2D to cross product with.
+	/// @return The magnitude of a vector (the cross product of the 3D version of these two vectors) at a right angle to both values.
+	constexpr float crossProduct( Vector2D const & rhs ) const noexcept { return ( x * rhs.y ) - ( y * rhs.x ); }
+	constexpr float dotProduct( Vector2D const & rhs ) const noexcept { return x * rhs.x + y * rhs.y; }
+
+	constexpr Vector2D & operator+=( Vector2D const & rhs ) noexcept { x += rhs.x; y += rhs.y; return *this; }
+	constexpr Vector2D & operator-=( Vector2D const & rhs ) noexcept { x -= rhs.x; y -= rhs.y; return *this; }
+
+	friend constexpr Vector2D operator+( Vector2D lhs, Vector2D const & rhs ) noexcept { return lhs += rhs; }
+	friend constexpr Vector2D operator-( Vector2D lhs, Vector2D const & rhs ) noexcept { return lhs -= rhs; }
+	friend constexpr Vector2D operator-( Vector2D lhs ) noexcept { lhs.x = -lhs.x; lhs.y = -lhs.y; return lhs; }
+
+	/// @brief An unsided non-assigning scalar multiplication.
 	/// @detail The compiler, when reading operators, doesn't consider arguments in any specific order (so writing float * Vector2D is exactly the same as writing Vector2D * float to the compiler). When interpreting operators, it considered operator overrides of the lhs first if the lhs has a valid override, it'll call lhs.operator*(rhs), if the lhs has no valid operator overrides, then it considers global operator overrides, however it never tries to find any overrides in the rhs, so even if the right hand symbol has a perfectly valid operator override, it will never call rhs.operator*(lhs). As of such, by writing this as a friend function (friend functions are actually global unless specified otherwise and only connects to the class it's in in that it can access private members, so this function can't actually be called via Vector2D.operator*())
 	/// @param lhs
 	/// @param rhs
 	/// @return a Vector2D with all values multiplied by rhs.
-	friend Vector2D operator*( Vector2D lhs, float rhs );
+	friend constexpr Vector2D operator*( Vector2D const & lhs, float rhs ) noexcept { return lhs.scalar( rhs ); }
+
 	//	friend Vector2D operator/( Vector2D lhs, float rhs );
 };
