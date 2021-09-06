712ec903bc8152577d76228c99f26cea069ced97
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-09-06 20:24:46 +1000

    Modified GameObject::render to more accurately render its collider.

--- GameObject.cpp
+++ GameObject.cpp
@@ -22,14 +22,22 @@ void GameObject::render( unsigned long, AssetManager * assets, RenderEngine * re
 {
 	if (Config::displayColliders)
 	{
-		Transform2D inheritedTransform = *transform();
+		Transform2D inheritedTransform{};
 		inheritedTransform.position = getCollider()->getWorldPosition();
 		//		constexpr float ANTI_THICC_COEFFICIENT = 0.01f;
 		inheritedTransform.rotation = 0;
-		inheritedTransform.scale.x /= getCollider()->extentsOfCollider().getSize().x;
-		inheritedTransform.scale.y /= getCollider()->extentsOfCollider().getSize().y;
+		inheritedTransform.scale = transform()->scale;
+		Vector2D colliderScale{};
+		Texture * colliderTexture = assets->getAsset<Texture>( getCollider()->getTextureID() );
+		colliderScale.x = getCollider()->extentsOfCollider().getSize().x / 2;
+		colliderScale.y = getCollider()->extentsOfCollider().getSize().y / 2;
+		inheritedTransform.position.x -= colliderScale.x / 2;
+		inheritedTransform.position.y -= colliderScale.y / 2;
+		inheritedTransform.scale.x += colliderScale.x / colliderTexture->getSize().width();
+		inheritedTransform.scale.y += colliderScale.y / colliderTexture->getSize().height();
+		//		std::cout << inheritedTransform.scale.x << " " << inheritedTransform.scale.y << std::endl;
 
-		renderer->renderTexture( *transform(), *assets->getAsset<Texture>( getCollider()->getTextureID() ) );
+		renderer->renderTexture( inheritedTransform, *colliderTexture );
 	}
 
 	renderer->renderTexture( *transform(), *assets->getAsset<Texture>( textureID() ) );
