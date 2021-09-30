e1aa1f460f9afbfbbff84817f6ecd679c8bd9e98
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-09-30 12:03:37 +1000

    Added BoardPosition functions, to represent the position (or target position) of the gem in boardspace.

--- GemObject.h
+++ GemObject.h
@@ -4,8 +4,14 @@
 class GemObject : GameObject
 {
 	GemType _gemType;
-
+	Coordinates _targetBoardPosition;
+	float _lerpSpeed;
 public:
 	GemType getGemType();
 	void setGemType( GemType newType );
+
+	Vector2D getBoardPosition();
+	float getLerpSpeed();
+	void setLerpSpeed( float );
+	void moveToBoardPosition( Coordinates position );
 };
