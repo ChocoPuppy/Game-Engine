1f2db9037b6f48e997596ff3c89e94d6fae50ed3
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-09-30 12:13:18 +1000

    Added functions for getting all world gems (middle of screen) and edge gems (on the boundary of the screen). Added a function for determining if you were to slide a point in a direction, where said point would stop.

--- GemsScene.h
+++ GemsScene.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Scene.h"
 #include <set>
+#include "TransversalDirections.h"
+#include "Vector2D.h"
 class GemObject;
 class GemsScene : Scene
 {
 public:
 	GemsScene();
 
-	std::set<GemObject *> getWorldGems();
+	std::set<std::weak_ptr<GemObject>> getWorldGems();
+	std::map<TransversalDirection, std::vector<std::weak_ptr<GemObject>>> getEdgeGems();
+
+	Coordinates slideCoordinatesUntilCollision( Coordinates origin, Vector2D direction );
 };
