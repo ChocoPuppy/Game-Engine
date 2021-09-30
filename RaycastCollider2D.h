519a53178c91b095f1abb95161a4b161f11880f2
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-09-30 17:20:45 +1000

    Created LineCollider2D as a way to detect if things exist on a specific line, using the transform as its origin and the direction as the direction it goes in and the extents to which it will go before stopping.

--- RaycastCollider2D.h
+++ RaycastCollider2D.h
@@ -1,5 +1,21 @@
 #pragma once
 #include "ICollider.h"
-class RaycastCollider2D
+namespace Collision
 {
-};
+	class LineCollider2D : public ICollider
+	{
+		Vector2D _direction;
+		bool _isRay;
+	public:
+
+		Vector2D getDirection() const;
+		//When doing collisions for this object, don't collide with objects past the closest collided one.
+		bool isRay() const;
+
+		void setDirection( Vector2D newDirection );
+		void setIsRay( bool isRay );
+		virtual std::string getTextureID() const override;
+		virtual Rect2D extentsOfCollider() const override;
+		virtual Vector2D getSupportPointOf( Vector2D point ) const override;
+	};
+}
