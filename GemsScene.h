c6501f1e3edae0e9c7f92bcd3a418512209e15a1
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-13 16:34:29 +1100

    Added a function for getting the map size. gems that go past these boundaries are to be destroyed.

--- GemsScene.h
+++ GemsScene.h
@@ -9,8 +9,12 @@ class GemsScene : Scene
 public:
 	GemsScene();
 
+	Vector2D getMapBounds() const noexcept;
+
 	std::set<std::weak_ptr<GemObject>> getWorldGems();
 	std::map<TransversalDirection, std::vector<std::weak_ptr<GemObject>>> getEdgeGems();
 
-	Coordinates slideCoordinatesUntilCollision( Coordinates origin, Vector2D direction );
+	//	Coordinates slideCoordinatesUntilCollision( Coordinates origin, Vector2D direction );
+private:
+	static constexpr Vector2D _MAP_BOUNDS = { 10,10 };
 };
