2ef2803e69d69572d88e2f16274dca9bc20eba1c
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-13 16:31:16 +1100

    Added declaration for GemType::toString, it takes the type and returns a string representation of it.
    Added declaration for GemType::toTextureID, it takes a gem type and provides what should be a valid texture ID representing that type.

--- GemType.cpp
+++ GemType.cpp
@@ -1,1 +1,30 @@
 #include "GemType.h"
+
+std::string GemType::toString( GemType type )
+{
+	switch (type)
+	{
+	case NONE:
+		return "None";
+	case RED:
+		return "Red";
+	case BLUE:
+		return "Blue";
+	case GREEN:
+		return "Green";
+	case WHITE:
+		return "White";
+	case RAINBOW:
+		return "Rainbow";
+	case BOMB:
+		return "Bomb";
+	default:
+		return "Unknown";
+	}
+}
+
+std::string GemType::toTextureID( GemType type )
+{
+	static const std::string TEXTURE_PATH_CONSTANT = "Textures.Gem.";
+	return TEXTURE_PATH_CONSTANT + toString( type );
+}
