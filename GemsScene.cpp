646194b6c8a6a09de050e386f61bfc1b31b4cbe5
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-13 16:16:50 +1100

    Created .cpp files for objects missing them.

---
+++ GemsScene.cpp
@@ -0,0 +1,14 @@
+#include "GemsScene.h"
+#include "GemObject.h"
+GemsScene::GemsScene() : Scene( "Gem scene" )
+{
+	auto * newGem = new GemObject( "Obj1" );
+	newGem->setGemType( GemType::BLUE );
+	newGem->rawMove( { 5,5 } );
+	addGameObject( newGem );
+}
+
+Vector2D GemsScene::getMapBounds() const
+{
+	return _MAP_BOUNDS;
+}
