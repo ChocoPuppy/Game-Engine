008daa3af0cab331137037169926913c9d9352b2
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-20 12:24:48 +1100

    Changed getEdgeGems to return an edge for a given direction instead of returning an entire map. added private storage pointers for what gems are where.

--- GemsScene.h
+++ GemsScene.h
@@ -12,11 +12,14 @@ public:
 	Vector2D getMapBounds() const noexcept;
 
 	std::set<std::weak_ptr<GemObject>> getWorldGems();
-	std::map<TransversalDirection, std::vector<std::weak_ptr<GemObject>>> getEdgeGems();
+	//Gems are stored leftmost/upmost to rightmost/downmost
+	std::vector<std::weak_ptr<GemObject>> getEdgeGems( TransversalDirection direction );
 
 	virtual void update( SDL::Window * window ) override;
 
 	//	Coordinates slideCoordinatesUntilCollision( Coordinates origin, Vector2D direction );
 private:
 	static constexpr Vector2D _MAP_BOUNDS = { 10,10 };
+	std::set<std::weak_ptr<GemObject>> _worldGems;
+	std::map<TransversalDirection, std::vector<std::weak_ptr<GemObject>>> _edgeGems;
 };
