750a661432461041dab217e51f006450566bc36b
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-28 12:52:39 +1100

    Added a custom copy constructor to Coordinates since the usage of reference types invalidates the default version.

--- Coordinates.h
+++ Coordinates.h
@@ -8,5 +8,6 @@ public:
 	int & y;
 
 	constexpr Coordinates( int x = 0, int y = 0 ) : std::pair<int, int>( x, y ), x( first ), y( second ) {};
+	constexpr Coordinates( Coordinates const & other ) : Coordinates( other.x, other.y ) {};
 	explicit Coordinates( Vector2D const & vector2D );
 };
