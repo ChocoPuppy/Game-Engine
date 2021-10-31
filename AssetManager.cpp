c35d8955c4816d0ff68469c48d0000a86b355fe6
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-31 22:17:18 +1100

    AssetManager now loads textures as surfaces that can be used to construct textures.

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -1,22 +1,22 @@
 #include "AssetManager.h"
 #include "AnimatedTexture.h"
+#include "SurfaceWrapper.h"
 AssetManager::AssetManager( SDL::Renderer * renderer )
 {
-	_loadAsset<Texture>( "Texture.Dino", "Assets/dino.png", renderer );
+	_loadAsset<Surface>( "Texture.Dino", "Assets/dino.png" );
 	//	_assets["Texture.Dino"] = new Texture( "Assets/dino.png", renderer );
 
-	_loadAsset<Texture>( "Texture.Debug.Rectangle", "Assets/rectangle.png", renderer );
+	_loadAsset<Surface>( "Texture.Debug.Rectangle", "Assets/rectangle.png" );
 
-	_loadAsset<Texture>( "Texture.Debug.Circle", "Assets/circle.png", renderer );
+	_loadAsset<Surface>( "Texture.Debug.Circle", "Assets/circle.png" );
 
-	constexpr int frameCount = 10;
-	constexpr unsigned long frameDurationMilliseconds = 100;
-	_loadAsset<AnimatedTexture>( "Texture.Dino.Walking", "Assets/dino.walking.png", renderer, frameCount, frameDurationMilliseconds );
+	_loadAsset<Surface>( "Texture.Dino.Walking", "Assets/dino.walking.png" );
+
 	//	_assets["Texture.Dino.Walking"] = new AnimatedTexture( "Assets/dino.walking.png", renderer, frameCount, frameDurationMilliseconds );
 
-	_loadAsset<AnimatedTexture>( "Texture.Player.Idle", "Assets/player.idle.png", renderer, frameCount, frameDurationMilliseconds );
-	_loadAsset<AnimatedTexture>( "Texture.Player.Walking", "Assets/player.walking.png", renderer, frameCount, frameDurationMilliseconds );
-	_loadAsset<AnimatedTexture>( "Texture.Player.Running", "Assets/player.running.png", renderer, frameCount, frameDurationMilliseconds );
+	_loadAsset<Surface>( "Texture.Player.Idle", "Assets/player.idle.png" );
+	_loadAsset<Surface>( "Texture.Player.Walking", "Assets/player.walking.png" );
+	_loadAsset<Surface>( "Texture.Player.Running", "Assets/player.running.png" );
 }
 
 AssetManager::~AssetManager()
