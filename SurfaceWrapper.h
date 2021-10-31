f0d7bfdc14ff4fb6f092d84495a5d487fcf59f80
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-10-31 22:22:09 +1100

    SurfaceWrapper now has an appropriate constructor.

--- SurfaceWrapper.h
+++ SurfaceWrapper.h
@@ -2,4 +2,7 @@
 #include "Surface.h"
 #include "Asset.h"
 class Surface : public SDL::Surface, public Asset
-{};
+{
+	Surface( std::string ID, std::string path ) : Asset( ID ), SDL::Surface( path )
+	{}
+};
