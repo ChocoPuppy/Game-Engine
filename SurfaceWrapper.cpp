f8d7828082f1d610f4f58b21cd2d099924270f67
Pwrocks5648 <7753274+ChocoPuppy@users.noreply.github.com>
2021-11-10 01:45:21 +1100

    Removed unused definitions from SurfaceWrapper.

--- SurfaceWrapper.cpp
+++ SurfaceWrapper.cpp
@@ -1,16 +1,1 @@
 #include "SurfaceWrapper.h"
-
-bool Surface::isAnimation() const
-{
-	return getAnimationProperties();
-}
-
-OptionalAnimProperties Surface::getAnimationProperties() const
-{
-	return _animationProperties;
-}
-
-void Surface::setAnimationProperties( OptionalAnimProperties newProperties )
-{
-	_animationProperties = newProperties;
-}
